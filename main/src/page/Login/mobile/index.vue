e223afaa11464590ec43754a039d84ce5d62cdeb
李铭辉 <1018596741@qq.com>
2021-03-29 23:07:36 +0800

    feat: 新增登陆页背景

--- main/src/page/Login/mobile/index.vue
+++ main/src/page/Login/mobile/index.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>mobile</div>
+    <div class="fixed inset-0 w-screen h-screen flex justify-center items-center bgAnimate" >
+    </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
@@ -11,3 +12,24 @@ export default defineComponent({
     },
 })
 </script>
+<style>
+.bgAnimate {
+    overflow: hidden;
+    background-size: 400%;
+    background-image: linear-gradient(125deg,#F44336,#E91E63,#9C27B0,#3F51B5,#2196F3);
+    font-family: "montserrat";
+    animation: bganimation 5s infinite;
+}
+
+@keyframes bganimation{
+    0%{
+        background-position: 0% 50%;
+    }
+    50%{
+        background-position: 100% 50%;
+    }
+    100%{
+        background-position: 0% 50%;
+    }
+}
+</style>
