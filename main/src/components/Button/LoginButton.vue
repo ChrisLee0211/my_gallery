d02aa3705f12755ad955b0b625557fabbb10de64
李铭辉 <1018596741@qq.com>
2021-04-01 23:39:13 +0800

    feat: 改css动画帧

--- main/src/components/Button/LoginButton.vue
+++ main/src/components/Button/LoginButton.vue
@@ -24,6 +24,7 @@ export default defineComponent({
 })
 </script>
 <style>
+
 .login-btn-active {
     display: inline-block;
     padding: 10px;
@@ -34,13 +35,39 @@ export default defineComponent({
     position: absolute;
     left: 0; top: 0; right: 0; bottom: 0;
     border: 2px solid #cd0000;
-    animation: borderAround 1.5s infinite linear;    
+    animation: borderAround 4s infinite linear;    
 }
 @keyframes borderAround {
-   0% {clip-path: polygon(0% 50%,0% 0%, 0% 50%);};
-   25% {clip-path: polygon(50% 0%, 100% 0%, 100% 50%,);}
-   50% {clip-path: polygon(100% 50%, 100% 100%, 50% 100%,);}
-   75% {clip-path: polygon(50% 100%, 0 100%, 0% 50,);}
-   100% {clip-path: polygon(0% 50%, 0% 0%, 50% 0%);};
+
+   0% {
+       clip-path: inset(0% 75% 25% 0%);
+    }
+   15% {
+       clip-path: inset(0% 25% 75% 0%);
+    }
+   30% {
+       clip-path: inset(0% 0% 75% 25%);
+    }
+   45% {
+       clip-path: inset(0% 0% 25% 75%);
+       }
+   60% {
+       clip-path: inset(25% 0% 0% 75%);
+       }
+   75% {
+       clip-path: inset(75% 0% 0% 25%);
+       }
+   90% {
+       clip-path: inset(75% 25% 0% 0%);
+       }
+   100% {
+       clip-path: inset(25% 75% 0% 0%);
+       }
+   /* 100% {
+       clip-path: inset(0% 75% 25% 0%);
+       } */
+   /* 100% {
+       clip-path: inset(0% 75% 75% 0%);
+       } */
 }
 </style>
