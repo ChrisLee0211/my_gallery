c1320b13a892d068c65a2a7e786cff8b1187fd2d
李铭辉 <1018596741@qq.com>
2021-04-05 11:34:02 +0800

    feat: 完成移动端登陆按钮

--- main/src/components/Button/LoginButton.vue
+++ main/src/components/Button/LoginButton.vue
@@ -1,6 +1,12 @@
 <template>
-    <div :class="className">
-        <div class="rounded flex items-center justify-center bg-indigo-700 shadow-md text-white " :style="`width:${width};height:${height}`">
+    <div class="relative" :style="baseSyleSize">
+        <div 
+        :class="className"
+        :style="baseSyleSize">
+        </div>
+        <div
+            class="absolute inset-2 cursor-pointer z-10 rounded flex items-center justify-center bg-indigo-700 shadow-md text-white"
+        >
             <slot />
         </div>
     </div>
@@ -9,27 +15,30 @@
 import { defineComponent, ref, computed } from 'vue'
 
 export default defineComponent({
-    name:'login-button',
+    name: 'login-button',
     props: {
-        loading:{
-            type:Boolean,
-            default:false
+        loading: {
+            type: Boolean,
+            default: false
         },
-        width:{
-            type:String,
-            default:'120px'
+        width: {
+            type: String,
+            default: '120px'
         },
-        height:{
-            type:String,
+        height: {
+            type: String,
             default: "30px",
         }
     },
-    setup(props,ctx) {
-        const className = computed(()=>{
-            if(props.loading) return 'login-btn-active'
+    setup(props, ctx) {
+        const className = computed(() => {
+            if (props.loading) return 'login-btn-active'
             return 'login-btn'
         });
-        return {className}
+        const baseSyleSize = computed(() => {
+            return `width:${parseInt(props.width)+15}px;height:${parseInt(props.height)+15}px`
+        })
+        return { className,baseSyleSize }
     },
 })
 </script>
@@ -37,30 +46,41 @@ export default defineComponent({
 .login-btn {
     padding: 10px;
     box-sizing: border-box;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
 }
 .login-btn-active {
     border: 2px solid lightblue;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
     border-radius: 0.25rem;
     padding: 10px;
+    z-index: 9;
     box-sizing: border-box;
-    animation: broderAround 2s linear 0s infinite normal;   
+    animation: broderAround 2s linear 0s infinite normal;
 }
 
 @keyframes broderAround {
     0% {
-        clip-path: circle(40% at 0 0) ;
+        clip-path: circle(40% at 0 0);
     }
-    25%{
-        clip-path: circle(40% at 100% 0) ;
+    25% {
+        clip-path: circle(40% at 100% 0);
     }
-    50%{
-        clip-path: circle(40% at 100% 100%) ;
+    50% {
+        clip-path: circle(40% at 100% 100%);
     }
-    75%{
-        clip-path: circle(40% at 0% 100%) ;
+    75% {
+        clip-path: circle(40% at 0% 100%);
     }
     100% {
-         clip-path: circle(40% at 0 0) ;
+        clip-path: circle(40% at 0 0);
     }
 }
 </style>
--- main/src/page/Home/index.vue
+++ main/src/page/Home/index.vue
@@ -3,10 +3,9 @@
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '../../components/HelloWorld.vue'
 export default defineComponent({
   components: {
-    HelloWorld
+    
   },
   setup() {
     const msg = 'home'
