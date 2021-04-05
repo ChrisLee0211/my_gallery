adb05519945552747aabf394258cc449dfd1395b
李铭辉 <1018596741@qq.com>
2021-04-05 22:13:13 +0800

    upd:mix自身样式和tailwind

--- main/src/components/Button/LoginButton.vue
+++ main/src/components/Button/LoginButton.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="relative" :style="baseSyleSize">
         <div 
-        :class="className"
+        :class="`absolute inset-0 rounded z-10 ${activeClass}`"
         :style="baseSyleSize">
         </div>
         <div
-            class="absolute inset-2 cursor-pointer z-10 rounded flex items-center justify-center bg-indigo-700 shadow-md text-white"
+            :class="`absolute inset-2 cursor-pointer z-20 rounded flex items-center justify-center bg-indigo-700 shadow-md text-white`"
         >
             <slot />
         </div>
@@ -28,41 +28,30 @@ export default defineComponent({
         height: {
             type: String,
             default: "30px",
+        },
+        size: {
+            type: String,
+            default: 'small'
         }
     },
     setup(props, ctx) {
-        const className = computed(() => {
+        const activeClass = computed(() => {
             if (props.loading) return 'login-btn-active'
             return 'login-btn'
         });
         const baseSyleSize = computed(() => {
             return `width:${parseInt(props.width)+15}px;height:${parseInt(props.height)+15}px`
         })
-        return { className,baseSyleSize }
+        return { activeClass,baseSyleSize }
     },
 })
 </script>
 <style>
 .login-btn {
     padding: 10px;
-    box-sizing: border-box;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
 }
 .login-btn-active {
     border: 2px solid lightblue;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    border-radius: 0.25rem;
-    padding: 10px;
-    z-index: 9;
-    box-sizing: border-box;
     animation: broderAround 2s linear 0s infinite normal;
 }
 
