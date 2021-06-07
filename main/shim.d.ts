c68130563a18b55ac82a480db6e4bbfaa37fb319
liminghui2 <liminghui2@joyy.sg>
2021-06-07 17:59:30 +0800

    feat: 新增回到顶部按钮在layout

--- main/shim.d.ts
+++ main/shim.d.ts
@@ -3,8 +3,3 @@ declare module '*.vue' {
   const componentOptions: ComponentOptions
   export default componentOptions
 }
-
-declare module 'animate.css' {
-  import {PluginInstallFunction} from 'vue';
-  export const install:PluginInstallFunction
-}
--- main/src/page/Layout/index.vue
+++ main/src/page/Layout/index.vue
@@ -1,6 +1,12 @@
 <template>
     <pc-menu v-if="isPC" />
     <mobile-menu v-else />
+    <div 
+    v-if="isPC" 
+    :class="`fixed right-8 bottom-12 rounded-full shadow-lg`"
+    >
+        <BackToTop  />
+    </div>
     <router-view />
 </template>
 <script lang="ts">
@@ -8,10 +14,13 @@ import { defineComponent, computed } from 'vue'
 import { useStore } from '../../store/index'
 import PcMenu from '../../components/Menu/pc.vue'
 import MobileMenu from '../../components/Menu/mobile.vue'
+import BackToTop from '../../components/BackToTop/index.vue';
+
 export default defineComponent({
     components:{
         'pc-menu':PcMenu,
-        'mobile-menu':MobileMenu
+        'mobile-menu':MobileMenu,
+        'BackToTop':BackToTop
     },
     setup() {
         const store = useStore();
