b64700276ac43b971443f2838f7c05f63caf4261
liminghui2 <liminghui2@joyy.sg>
2021-06-07 17:59:07 +0800

    feat: 新增回到顶部组件

---
+++ main/src/components/BackToTop/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div 
+        class="rounded-full w-12 h-12 bg-black shadow-md flex items-center justify-center cursor-pointer" 
+        @click="clickToTop">
+        <Icon :name="`topArrow`" :customClass="`text-white`"/>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Icon from '../Icon/index.vue';
+
+export default defineComponent({
+    components:{
+        Icon:Icon
+    },
+    setup() {
+        const clickToTop = () => {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth',
+            });
+        }
+        return {
+            clickToTop
+        }
+    },
+})
+</script>
