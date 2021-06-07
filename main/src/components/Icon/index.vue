04134f0058648762404f6901fe352fae6fe30679
liminghui2 <liminghui2@joyy.sg>
2021-06-07 17:58:37 +0800

    feat: 新增icon组件

---
+++ main/src/components/Icon/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <svg :class="customClass" :width="`${size}`" :height="`${size}`">
+        <use v-if="name" :xlink:href="`#icon${name}`" />
+    </svg>
+</template>
+<script lang="ts">
+export default {
+    name:'svg-icon',
+    props:{
+        name:{
+            type:String,
+            default:undefined
+        },
+        customClass:{
+            type:String,
+            default:"svg-icon"
+        },
+        size:{
+            type:Number,
+            default:20
+        }
+    },
+}
+</script>
